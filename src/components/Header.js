763f6474d3ff2b92396a2d24ce224e852fd13d84
Gwenaëlle Boussès <cgwenaelle@hotmail.com>
2024-01-03 16:37:11 +0100

    views and components

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <nav>
+            <img className="logo" src="Logo.png" alt="logo de 'trouve ton artisan'"></img>
+        </nav>
+    )
+}
+
+export default Header
