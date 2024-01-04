1a81fbcf77349f7dd4475b958e282cf7ebdccc40
Gwenaëlle Boussès <cgwenaelle@hotmail.com>
2024-01-04 15:52:46 +0100

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,32 @@
 const Footer = () => {
     return (
-        <div></div>
+        <footer className="bg-primary text-white position-absolute bottom-0 w-100">
+            <div className="container">
+                <div>
+                    <address>
+                        <h2>Lyon</h2>
+                        <i class="fa-solid fa-location-dot"></i>
+                        <p>101 Cours Charlemagne</p>
+                        <p>CS 20033</p>
+                        <p>69269 LYON CEDEX 02</p>
+                        <p>FRANCE</p>
+                        <i class="fa-solid fa-phone"></i>
+                        <a href="tel:04 26 73 4000"> +33 (0)4 26 73 4000</a>
+                    </address>
+                </div>
+                <hr></hr>
+                <div className="d-flex justify-content-between">
+                    <a>Mentions légales</a>
+                    <a>Données personnelles</a>
+                    <a>Accessibilité : conforme</a>
+                    <a>Presse</a>
+                    <a>Marchés publics</a>
+                    <a>Venir à la région</a>
+                    <a>Contacts</a>
+                    <a>Gestions des cookies</a>
+                </div>
+            </div>
+        </footer>
     )
 }
 
