bd526ec06003b6296ba564158efd8c7e75bc36ee
Gwenaëlle Boussès <cgwenaelle@hotmail.com>
2024-01-06 18:29:46 +0100

    search by name, location and specialty

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './views/Home';
 import List from './views/List-result-search';
+import Results from "./views/Search-results";
 import Details from './views/Details';
 import Page404 from './views/404';
 import './App.scss';
+import list from './datas/datas.json'
+
 
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("")
+
+  const handleSearch = (term) => {
+    setSearchTerm(term)
+  }
+
   return (
     <div className="App">
-      <Header />
+      <Header onSearch={handleSearch} />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/searchresults/:artisanCategory" element={<List />} />
+        <Route path="/results" element={<Results data={list} searchTerm={searchTerm} />} />
         <Route path="/details/:artisanId" element={<Details />} />
+        <Route path="/:artisanCategory" element={<List />} />
         <Route path="/*" element={<Page404 />} />
       </Routes>
       <Footer />
---
+++ src/views/Search-results.js
@@ -0,0 +1,26 @@
+import Card from "../components/Card-artisan"
+
+const Results = ({ data, searchTerm }) => {
+    const results = data.filter(artisan => artisan.specialty.toLowerCase().includes(searchTerm.toLowerCase()) || artisan.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    artisan.location.toLowerCase().includes(searchTerm.toLowerCase()))
+    const artisans = results.map(artisan => {
+        return <Card
+            name={artisan.name}
+            specialty={artisan.specialty}
+            location={artisan.location}
+            btn="En savoir plus"
+            key={artisan.id}
+            id={artisan.id} />
+    })
+    return (
+        <div className="page">
+            <h1>Le résultat de votre recherche</h1>
+            <div className="sep1"></div>
+            <div className='row list-card'>
+                {artisans}
+            </div>
+       </div>
+    )
+}
+
+export default Results
